1e4a51c06ad4032f2759912a1511fc27ddeead08
mezeiser <mezardini@gmail.com>
2023-08-30 16:51:23 +0100

    worked on settings

--- footbuddy/settings.py
+++ footbuddy/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_SSL = True
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'olaitanola622@gmail.com'
-EMAIL_HOST_PASSWORD = 'dbcesbgrosvdozsq'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 
 
